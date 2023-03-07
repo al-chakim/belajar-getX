b0d6e8578a738ee85d98176a55244071dcb5081c
Muchammad Amru Al-Chakimi <amrualchakim@gmail.com>
2023-03-07 09:04:48 +0700

    tes sncak bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,2 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get_apps/routes/route_name.dart';
-import './routes/route_page.dart';
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      initialRoute: RuteNama.home,
-      getPages: App.AppPages,
-    );
-  }
-}
